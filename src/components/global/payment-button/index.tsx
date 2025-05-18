599fa8d29eb9cb23a9e93d9abf40be02ea58183c
Piyush patil <098piyushpatil@gmail.com>
2025-05-18 19:20:06 +0530

    Implement subscription functionality: add useSubscription hook, create PaymentButton component, and set up payment API route with Stripe integration.

--- src/components/global/payment-button/index.tsx
+++ src/components/global/payment-button/index.tsx
@@ -1,18 +1,23 @@
+'use client'
 import { Button } from '@/components/ui/button'
+import { useSubscription } from '@/Hook/use-subscription'
 import React from 'react'
+import Loader from '../loader'
+import { CreditCardIcon, Loader2 } from 'lucide-react'
 
 type Props = {}
 
 const PaymentButton = (props: Props) => {
-  //WIP : get their subscription details
-  //WIP : Loading State
   
-  useSubscription()
+  const { onSubscribe , isProcessing} = useSubscription()
   
   return ( 
     <Button
+      disabled={isProcessing}
+      onAbort={onSubscribe}
       className='bg-gradient-to-r text-white font-bold rounded-full from-[#9685DB] via-[#9434E6] to-[#CC38D4]'
     >
+      {isProcessing ? <Loader2 className='animate-spin '/> : <CreditCardIcon/>}
       Upgrade
     </Button>
     )
